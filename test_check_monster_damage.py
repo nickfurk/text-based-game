bc08f4a9a26672a114a05ec1c209d616f9b009f7
April Cheng <april.sh.cheng@gmail.com>
2021-03-19 10:46:13 -0700

    Edited test_check_monster_damage.py

--- test_check_monster_damage.py
+++ test_check_monster_damage.py
@@ -6,8 +6,8 @@ from game import check_monster_damage
 class TestCheckMonsterDamage(TestCase):
     @patch('game.MONSTER_DAMAGE', return_value={1: {'level': 1, 'damage': 10}, 2: {'level': 2, 'damage': 10 + 5},
                                                 3: {'level': 3, 'damage': 10 + 5 * 2}})
-    def test_check_monster_damage_change(self, mock_damage):
-        player = {'name': 'Paul', 'class': 'Warrior', 'hp': 20, 'position': [0, 0], 'level': 2, 'experience': 400,
+    def test_check_monster_damage_change_equivalent_to_level_2(self, mock_damage):
+        player = {'class': 'Warrior', 'hp': 20, 'position': [0, 0], 'level': 2, 'experience': 400,
                   'category': 'player', 'class_dictionary': {'level': 2, 'level_name': 'Apprentice Warrior',
                                                              'experience_needed': 500, 'attack_name': 'Threaten',
                                                              'max_hp': 20, 'base_damage_min': 7, 'base_damage_max': 12,
@@ -15,6 +15,20 @@ class TestCheckMonsterDamage(TestCase):
         monster = {'name': 'Zelda', 'type': 'Cat', 'category': 'monster', 'damage': 10}
         check_monster_damage(player, monster)
         expected = {'name': 'Zelda', 'type': 'Cat', 'category': 'monster', 'damage': 15}
+        actual = monster
+        self.assertEqual(expected, actual)
+
+    @patch('game.MONSTER_DAMAGE', return_value={1: {'level': 1, 'damage': 10}, 2: {'level': 2, 'damage': 10 + 5},
+                                                3: {'level': 3, 'damage': 10 + (5 * 2)}})
+    def test_check_monster_damage_change_equivalent_to_level_3(self, mock_damage):
+        player = {'class': 'Warrior', 'hp': 30, 'position': [0, 0], 'level': 3, 'experience': 1000,
+                  'category': 'player', 'class_dictionary': {'level': 3, 'level_name': 'Apprentice Warrior',
+                                                             'attack_name': 'Havens Hammer',
+                                                             'max_hp': 30, 'base_damage_min': 18, 'base_damage_max': 24,
+                                                             'accuracy_rate': 50}}
+        monster = {'name': 'Zelda', 'type': 'Cat', 'category': 'monster', 'damage': 15}
+        check_monster_damage(player, monster)
+        expected = {'name': 'Zelda', 'type': 'Cat', 'category': 'monster', 'damage': 20}
         actual = monster
         self.assertEqual(expected, actual)
 
