1bd18fbf2a88505f4306679156c53e0dfd6920bb
April Cheng <april.sh.cheng@gmail.com>
2021-03-18 21:28:05 -0700

    Added test_player_class_generator.py

---
+++ test_player_class_generator.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from unittest.mock import patch
+from game import player_class_generator
+
+
+class TestPlayerClassGenerator(TestCase):
+
+    @patch('builtins.input', side_effect=["2"])
+    def test_player_dictionary_class_gets_updated(self, mock_input):
+        player_dictionary = {"class": ""}
+        player_class_generator(player_dictionary)
+        actual = player_dictionary
+        expected = {"class": "Thief"}
+        self.assertEqual(actual, expected)
+
+    @patch('builtins.input', side_effect=["2"])
+    def test_return_correct_class(self, mock_input):
+        player_dictionary = {"class": ""}
+        actual = player_class_generator(player_dictionary)
+        expected = "Thief"
+        self.assertEqual(actual, expected)
