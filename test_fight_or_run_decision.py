ce202a41b90f30189b6bb19aa36fd8996398f1b2
Paul Seong Uk Yeon <syeon@my.bcit.ca>
2021-03-19 02:14:50 -0700

    Added fight_or_run_decision unit test (Not completed, third test is incomplete)

---
+++ test_fight_or_run_decision.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from unittest.mock import patch
+from game import fight_or_run_decision
+import game
+import io
+
+class TestFightOrRunDecision(TestCase):
+    @patch('game.input_checker', return_value='Yes')
+    def test_fight_or_run_decision_yes(self, mock_input):
+        monster = {'name': 'Paul'}
+        expected_output = 'Yes'
+        self.assertEqual(expected_output, fight_or_run_decision(monster))
+
+    @patch('game.input_checker', return_value='No')
+    def test_fight_or_run_decision_no(self, mock_input):
+        monster = {'name': 'Paul'}
+        expected_output = 'No'
+        self.assertEqual(expected_output, fight_or_run_decision(monster))
+
+    @patch('game.input_checker', side_effect=['', 'Yes'])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_fight_or_run_decision_error(self, mock_stdout, mock_input):
+        monster = {'name': 'Paul'}
+        expected_output = '\nYou have encountered Paul! Would you like to fight?\n\n' \
+                          ' is not a valid choice!, Please Choose again: \n'
+        fight_or_run_decision(monster)
+        self.assertEqual(expected_output, mock_stdout.get_value())
+
