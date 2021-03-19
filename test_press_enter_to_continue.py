01542663d33b381bd9211330ba0081f438d944f5
Paul Seong Uk Yeon <syeon@my.bcit.ca>
2021-03-18 21:32:39 -0700

    Renamed test_file to game_testing to avoid confusion. Add press_enter_to_continue unittest.

---
+++ test_press_enter_to_continue.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from unittest.mock import patch
+from game import press_enter_to_continue
+import io
+
+class TestPressEnterToContinue(TestCase):
+
+    @patch('builtins.input', side_effect=[''])
+    def test_press_enter_to_continue_output(self, mock_input):
+        actual = press_enter_to_continue()
+        expected_output = None
+        self.assertEqual(expected_output, actual)
+
+    @patch('builtins.input', side_effect=[''])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_press_enter_to_continue_correct(self, mock_stdout, mock_input):
+        expected_output = ''
+        press_enter_to_continue()
+        self.assertEqual(expected_output, mock_stdout.getvalue())
+
+    @patch('builtins.input', side_effect=['Okay', ''])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_press_enter_to_continue_error(self, mock_stdout, mock_input):
+        expected_output = 'Please press enter!\n'
+        press_enter_to_continue()
+        self.assertEqual(expected_output, mock_stdout.getvalue())
