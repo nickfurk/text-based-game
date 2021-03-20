70eb80d10ad71f7840b2a14f5cb0a72607d14342
April Cheng <april.sh.cheng@gmail.com>
2021-03-19 17:12:34 -0700

    Edited function test_battle_start.py

--- test_battle_start.py
+++ test_battle_start.py
@@ -6,7 +6,7 @@ from game import battle_start
 class TestBattleStart(TestCase):
 
     @patch('game.player_damage', return_value=5)
-    def test_player_goes_first_both_sides_lose_hp(self, mock_player_damage):
+    def test_player_attacks_first_both_sides_lose_hp(self, mock_player_damage):
         monster = {"name": "Zelda", "hp": 10, "damage": 5}
         player = {'name': 'Paul', 'class': 'Warrior', 'hp': 20, 'position': [0, 0], 'level': 1, 'experience': 0,
                   'category': 'player', 'class_dictionary': {'level': 1, 'level_name': 'Apprentice Warrior',
@@ -17,7 +17,7 @@ class TestBattleStart(TestCase):
         self.assertTrue(monster["hp"] != 10, player["hp"] != 20)
 
     @patch('game.player_damage', return_value=5)
-    def test_monster_goes_first_both_sides_lose_hp(self, mock_player_damage):
+    def test_monster_attacks_first_both_sides_lose_hp(self, mock_player_damage):
         monster = {"name": "Zelda", "hp": 10, "damage": 5}
         player = {'name': 'Paul', 'class': 'Warrior', 'hp': 20, 'position': [0, 0], 'level': 1, 'experience': 0,
                   'category': 'player', 'class_dictionary': {'level': 1, 'level_name': 'Apprentice Warrior',
@@ -28,7 +28,7 @@ class TestBattleStart(TestCase):
         self.assertTrue(monster["hp"] != 10, player["hp"] != 20)
 
     @patch('game.player_damage', return_value=10)
-    def test_player_attacks_first_monster_dies_no_hp(self, mock_player_damage):
+    def test_player_attacks_first_monster_dies(self, mock_player_damage):
         monster = {"name": "Zelda", "hp": 10, "damage": 5}
         player = {'name': 'Paul', 'class': 'Warrior', 'hp': 20, 'position': [0, 0], 'level': 1, 'experience': 0,
                   'category': 'player', 'class_dictionary': {'level': 1, 'level_name': 'Apprentice Warrior',
@@ -49,7 +49,7 @@ class TestBattleStart(TestCase):
         battle_start(player, monster, True)
         self.assertTrue(monster["hp"] == 0, player["experience"] == 100)
 
-    def test_monster_attacks_first_player_dies_no_hp(self):
+    def test_monster_attacks_player_dies_no_hp(self):
         monster = {"name": "Zelda", "hp": 10, "damage": 5}
         player = {'name': 'Paul', 'class': 'Warrior', 'hp': 1, 'position': [0, 0], 'level': 1, 'experience': 0,
                   'category': 'player', 'class_dictionary': {'level': 1, 'level_name': 'Apprentice Warrior',
