f86799f940516d4a7257b2f0c7a5da1bef4835c9
Paul Seong Uk Yeon <syeon@my.bcit.ca>
2021-03-19 23:35:28 -0700

    Add unittest for correct print when input is correct.

--- test_fight_or_run_decision.py
+++ test_fight_or_run_decision.py
@@ -17,6 +17,14 @@ class TestFightOrRunDecision(TestCase):
         expected_output = 'No'
         self.assertEqual(expected_output, fight_or_run_decision(monster))
 
+    @patch('game.input_checker', return_value='Yes')
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_fight_or_run_decision_correct_print(self, mock_stdout, mock_input):
+        monster = {'name': 'Paul'}
+        expected_output = '\nYou have encountered Paul! Would you like to fight?\n\n'
+        fight_or_run_decision(monster)
+        self.assertEqual(expected_output, mock_stdout.getvalue())
+
     @patch('game.input_checker', side_effect=['', 'Yes'])
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_fight_or_run_decision_error(self, mock_stdout, mock_input):
