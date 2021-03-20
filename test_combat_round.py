21e71932a13b54ef19665ced70e7a947d750b62a
Paul Seong Uk Yeon <syeon@my.bcit.ca>
2021-03-20 00:02:24 -0700

    Add fight to player death.

--- test_combat_round.py
+++ test_combat_round.py
@@ -14,9 +14,9 @@ class TestCombatRound(TestCase):
     @patch('game.run_or_fight_again', return_value="Yes")
     @patch('game.press_enter_to_continue', return_value='')
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_combat_round_fight_to_death(self, mock_stdout, mock_continue, mock_fight_again, mock_monster_run, mock_damage,
-                                  mock_attack_order, mock_decision, mock_roll_die):
-        monster = {"name": "Zelda", "hp": 1}
+    def test_combat_round_fight_to_death(self, mock_stdout, mock_continue, mock_fight_again, mock_monster_run,
+                                         mock_damage, mock_attack_order, mock_decision, mock_roll_die):
+        monster = {"name": "Zelda", "hp": 1, "damage": 5}
         player = {'name': 'Paul', 'class': 'Warrior', 'hp': 20, 'position': [0, 0], 'level': 1, 'experience': 0,
                   'category': 'player', 'class_dictionary': {'level': 1, 'level_name': 'Apprentice Warrior',
                                                              'experience_needed': 200, 'attack_name': 'Threaten',
@@ -24,6 +24,29 @@ class TestCombatRound(TestCase):
                                                              'accuracy_rate': 50}}
         expected_output = "Paul has done 1 damage to Zelda!\n" \
                           "Zelda has 0 hp left!\n\n"
+        combat_round(player, monster)
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    @patch('game.roll_die', return_value=1)
+    @patch('game.fight_or_run_decision', return_value="Yes")
+    @patch('game.battle_attack_order', return_value=True)
+    @patch('game.player_damage', return_value=1)
+    @patch('game.run_away_monster', return_value=False)
+    @patch('game.run_or_fight_again', return_value="Yes")
+    @patch('game.press_enter_to_continue', return_value='')
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_combat_round_player_dead(self, mock_stdout, mock_continue, mock_fight_again, mock_monster_run,
+                                      mock_damage, mock_attack_order, mock_decision, mock_roll_die):
+        monster = {"name": "Zelda", "hp": 5, "damage": 5}
+        player = {'name': 'Paul', 'class': 'Warrior', 'hp': 1, 'position': [0, 0], 'level': 1, 'experience': 0,
+                  'category': 'player', 'class_dictionary': {'level': 1, 'level_name': 'Apprentice Warrior',
+                                                             'experience_needed': 200, 'attack_name': 'Threaten',
+                                                             'max_hp': 20, 'base_damage_min': 7, 'base_damage_max': 12,
+                                                             'accuracy_rate': 50}}
+        expected_output = "Paul has done 1 damage to Zelda!\n" \
+                          "Zelda has 4 hp left!\n\n" \
+                          "Zelda has done 1 damage to Paul!\n" \
+                          "Paul has 0 hp left!\n\n"
         combat_round(player, monster)
         self.assertEqual(mock_stdout.getvalue(), expected_output)
 
