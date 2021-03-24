f7d715f5d7836675bedc0251bc22c52d2c1b8b7a
April Cheng <april.sh.cheng@gmail.com>
2021-03-23 22:25:34 -0700

    Edited random_name_generator

--- test_random_name_generator.py
+++ test_random_name_generator.py
@@ -1,17 +1,9 @@
 from unittest import TestCase
 from unittest.mock import patch
 from game import random_name_generator
-from coolname import generate
 
 
 class TestRandomNameGenerator(TestCase):
-
-    @patch('builtins.input', side_effect=["1"])
-    @patch('game.generate', side_effect=["Stirring Lyrebird"])
-    def test_select_generated_name(self, mock_generate, mock_input):
-        actual = random_name_generator()
-        expected = "Stirring Lyrebird"
-        self.assertEqual(actual, expected)
 
     @patch('builtins.input', side_effect=["2"])
     def test_select_choose_another_random_name(self, mock_input):
