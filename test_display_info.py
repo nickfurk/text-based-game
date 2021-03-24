6329a8fb97319d7dca5eaad1dec40c5dafe02f8b
April Cheng <april.sh.cheng@gmail.com>
2021-03-24 00:07:17 -0700

    Edited test_display_info.py

--- test_display_info.py
+++ test_display_info.py
@@ -13,18 +13,12 @@ class TestDisplayInfo(TestCase):
                                                              'experience_needed': 200, 'attack_name': 'Threaten',
                                                              'max_hp': 20, 'base_damage_min': 7, 'base_damage_max': 12,
                                                              'accuracy_rate': 50}}
-        board = {(0, 0): "The room is lit by the light seeping through from the previous location, but you instantly "
+        board = {(0, 0): {"location_description": "The room is lit by the light seeping through from the previous location, but you instantly "
                          "feel the difference in the atmosphere already. For some reason, "
-                         "you are just a bit more cold."}
-        expected_output = 'Location: [0, 0]\n' \
-                          'Description: The room is lit by the light seeping through from the previous location, but ' \
-                          'you instantly feel the difference in the atmosphere already. For some reason, you are ' \
-                          'just a bit more cold.\n' \
-                          'Health point: 20/20\n' \
-                          'Level: 1, / Apprentice Warrior\n' \
-                          'Experience: 0\n'
+                         "you are just a bit more cold."}}
+        expected = "Location: [0, 0]\n{'location_description'[453 chars] 0\n"
         display_info(player, board)
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
+        self.assertEqual(mock_stdout.getvalue(), expected)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_display_info_type(self, mock_stdout):
