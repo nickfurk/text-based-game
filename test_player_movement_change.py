c7036a53cb48d4ee09c695fc8bccee57fe4d3ff7
Paul Seong Uk Yeon <syeon@my.bcit.ca>
2021-03-27 14:12:29 -0700

    Revise test_player_movement_change unit test

--- test_player_movement_change.py
+++ test_player_movement_change.py
@@ -5,25 +5,25 @@ from game import player_movement_change
 class TestPlayerMovementChange(TestCase):
     def test_player_movement_change_list(self):
         current_position = [1, 2]
-        player_movement_change(current_position, "N")
+        player_movement_change(current_position, "North")
         self.assertEqual(type(current_position), list)
 
     def test_player_movement_change_north(self):
         current_position = [1, 2]
-        player_movement_change(current_position, "N")
+        player_movement_change(current_position, "North")
         self.assertEqual(current_position, [0, 2])
 
     def test_player_movement_change_south(self):
         current_position = [1, 2]
-        player_movement_change(current_position, "S")
+        player_movement_change(current_position, "South")
         self.assertEqual(current_position, [2, 2])
 
     def test_player_movement_change_west(self):
         current_position = [1, 2]
-        player_movement_change(current_position, "W")
+        player_movement_change(current_position, "West")
         self.assertEqual(current_position, [1, 1])
 
     def test_player_movement_change_east(self):
         current_position = [1, 2]
-        player_movement_change(current_position, "E")
+        player_movement_change(current_position, "East")
         self.assertEqual(current_position, [1, 3])
