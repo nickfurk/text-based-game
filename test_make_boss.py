6e2cccf7d8837394037e5dbffb7f9cf5c62a3b5e
April Cheng <april.sh.cheng@gmail.com>
2021-03-27 17:15:39 -0700

    Clean test_make_boss.py

--- test_make_boss.py
+++ test_make_boss.py
@@ -1,15 +1,13 @@
 from unittest import TestCase
 from unittest.mock import patch
 from game import make_boss
-import game
-import io
 
 
 class TestMakeBoss(TestCase):
     @patch('game.choice', return_value='Kalzeruth')
     def test_make_boss_correct_boss(self, mock_choice):
         expected_output = {'name': '\x1b[31mKalzeruth\x1b[0m', 'category': 'boss', 'hp': 60, 'damage': 20,
-                           'position': [15, 15],
+                           'position': [25, 25],
                            'attack_name': ['Massacre', 'Demolish', 'Torture', 'Execute', 'Harvest']}
         self.assertEqual(expected_output, make_boss())
 
