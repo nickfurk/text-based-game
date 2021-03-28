ac845c9f3bf8e3a6b018c2caf617fbacf5331d69
April Cheng <april.sh.cheng@gmail.com>
2021-03-27 17:05:02 -0700

    Edit test_game_win_art.py

--- test_game_win_art.py
+++ test_game_win_art.py
@@ -9,7 +9,7 @@ class TestGameWinArt(TestCase):
     @patch('builtins.input', side_effect=[""])
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_game_win_art_correct_print(self, mock_stdout, mock_input, mock_quit):
-        expected_output = "\n\n\nASCII ART HERE\n"
+        expected_output = "\n\n\nyou win!\n"
         game_win_art()
         self.assertEqual(expected_output, mock_stdout.getvalue())
 
