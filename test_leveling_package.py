03d5fb7e261a41c2cd678bb23253f55709178725
April Cheng <april.sh.cheng@gmail.com>
2021-03-27 17:11:11 -0700

    Edit test_leveling_package.py

--- test_leveling_package.py
+++ test_leveling_package.py
@@ -3,42 +3,7 @@ from game import leveling_package
 
 
 class TestLevelingPackage(TestCase):
-    def test_leveling_package_player_level_change_based_on_experience(self):
-        player = {'category': 'player',
-                  'class': 'Warrior',
-                  'class_dictionary': {'accuracy_rate': 50,
-                                       'attack_name': 'Threaten',
-                                       'base_damage_max': 12,
-                                       'base_damage_min': 7,
-                                       'experience_needed': 200,
-                                       'level': 1,
-                                       'level_name': 'Apprentice Warrior',
-                                       'max_hp': 20},
-                  'experience': 400,
-                  'hp': 20,
-                  'level': 2,
-                  'name': 'Leo',
-                  'position': [0, 0]}
-        leveling_package(player)
-        actual = player
-        expected = {'category': 'player',
-                    'class': 'Warrior',
-                    'class_dictionary': {'accuracy_rate': 50,
-                                         'attack_name': 'Power Crash',
-                                         'base_damage_max': 15,
-                                         'base_damage_min': 10,
-                                         'experience_needed': 700,
-                                         'level': 2,
-                                         'level_name': 'Knight',
-                                         'max_hp': 50},
-                    'experience': 400,
-                    'hp': 20,
-                    'level': 2,
-                    'name': 'Leo',
-                    'position': [0, 0]}
-        self.assertEqual(actual, expected)
-
-    def test_leveling_package_player_class_dictionary_change_based_on_experience(self):
+    def test_leveling_package_player_level_and_dictionary_change_based_on_experience(self):
         player = {'category': 'player',
                   'class': 'Warrior',
                   'class_dictionary': {'accuracy_rate': 50,
