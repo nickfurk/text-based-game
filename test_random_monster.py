8ab3f7af55e0983b25d7f48588d2d9fdbe7bd16b
April Cheng <april.sh.cheng@gmail.com>
2021-03-27 18:02:31 -0700

    Edit test_random_monster.py

--- test_random_monster.py
+++ test_random_monster.py
@@ -1,25 +1,25 @@
 from unittest import TestCase
 from unittest.mock import patch
 from game import random_monster
-import game
-import io
 
 
 class TestRandomMonster(TestCase):
+    @patch('game.randint', return_value=8)
     @patch('game.choice', side_effect=['Amputator', 'Ureh Caverns'])
-    def test_random_monster_level_one(self, mock_choice):
+    def test_random_monster_level_one(self, mock_choice, mock_random):
         player = {'name': 'Paul', 'class': 'Warrior', 'hp': 20, 'position': [0, 0], 'level': 1, 'experience': 0,
                   'category': 'player', 'class_dictionary': {'level': 1, 'level_name': 'Apprentice Warrior',
                                                              'experience_needed': 200, 'attack_name': 'Threaten',
                                                              'max_hp': 20, 'base_damage_min': 7, 'base_damage_max': 12,
                                                              'accuracy_rate': 50}}
         expected_output = {'name': '\x1b[33;1mAmputator\x1b[0m', 'type': 'Ureh Caverns', 'hp': 10,
-                           'category': 'monster', 'damage': 10, 'attack_name': ['Bite', 'Slash', 'Poisonous Trap']}
+                           'category': 'monster', 'damage': 8, 'attack_name': ['Bite', 'Slash', 'Poisonous Trap']}
         actual = random_monster(player)
         self.assertEqual(expected_output, actual)
 
+    @patch('game.randint', return_value=12)
     @patch('game.choice', side_effect=['Amputator', 'Ureh Caverns'])
-    def test_random_monster_level_two(self, mock_choice):
+    def test_random_monster_level_two(self, mock_choice, mock_random):
         player = {'category': 'player',
                   'class': 'Warrior',
                   'class_dictionary': {'accuracy_rate': 50, 'attack_name': 'Power Crash', 'base_damage_max': 15,
@@ -31,6 +31,6 @@ class TestRandomMonster(TestCase):
                   'name': '\u001b[32;1mplayer\u001b[0m',
                   'position': [0, 0]}
         expected_output = {'name': '\x1b[33;1mAmputator\x1b[0m', 'type': 'Ureh Caverns', 'hp': 17,
-                           'category': 'monster', 'damage': 15, 'attack_name': ['Bite', 'Slash', 'Poisonous Trap']}
+                           'category': 'monster', 'damage': 12, 'attack_name': ['Bite', 'Slash', 'Poisonous Trap']}
         actual = random_monster(player)
         self.assertEqual(expected_output, actual)
