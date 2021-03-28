c2bf075aa3239f16b7846dca93b15184f0db9e66
Paul Seong Uk Yeon <syeon@my.bcit.ca>
2021-03-27 19:09:07 -0700

    Revise random_name_generator unit test

--- test_random_name_generator.py
+++ test_random_name_generator.py
@@ -12,10 +12,11 @@ class TestRandomNameGenerator(TestCase):
         expected = "Stirring Lyrebird"
         self.assertEqual(actual, expected)
 
-    @patch('builtins.input', side_effect=["2"])
-    def test_random_name_generator_select_choose_another_random_name(self, mock_input):
+    @patch('builtins.input', side_effect=["2", "1"])
+    @patch('game.generate', side_effect=[['Random', 'Name'], ['Stirring', 'Lyrebird']])
+    def test_random_name_generator_select_choose_another_random_name(self, mock_generate, mock_input):
         actual = random_name_generator()
-        expected = "I would like to choose another one at random"
+        expected = "Stirring Lyrebird"
         self.assertEqual(actual, expected)
 
     @patch('builtins.input', side_effect=["3"])
@@ -27,8 +28,25 @@ class TestRandomNameGenerator(TestCase):
     @patch('builtins.input', side_effect=["1"])
     @patch('game.generate', side_effect=[['Stirring', 'Lyrebird']])
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_random_name_generator_correct_print(self, mock_stdout, mock_generate, mock_input):
-        expected = "\n\u001b[32;1mStirring Lyrebird\u001b[0m was created randomly! Would you like to choose your " \
+    def test_random_name_generator_choose_random_correct_print(self, mock_stdout, mock_generate, mock_input):
+        expected = "\u001b[32;1mStirring Lyrebird\u001b[0m was created randomly! Would you like to choose your " \
+                   "own name or create another at random?\n\n" \
+                   "{1: 'I would like to choose this name!'\n" \
+                   " 2: 'I would like to choose another one at random'\n" \
+                   " 3: 'I would like to choose my own name!'}\n"
+        random_name_generator()
+        self.assertEqual(expected, mock_stdout.getvalue())
+
+    @patch('builtins.input', side_effect=["2", "1"])
+    @patch('game.generate', side_effect=[['Random', 'Name'], ['Stirring', 'Lyrebird']])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_random_name_generator_choose_another_correct_print(self, mock_stdout, mock_generate, mock_input):
+        expected = "\u001b[32;1mRandom Name\u001b[0m was created randomly! Would you like to choose your " \
+                   "own name or create another at random?\n\n" \
+                   "{1: 'I would like to choose this name!'\n" \
+                   " 2: 'I would like to choose another one at random'\n" \
+                   " 3: 'I would like to choose my own name!'}\n\n" \
+                   "\u001b[32;1mStirring Lyrebird\u001b[0m was created randomly! Would you like to choose your " \
                    "own name or create another at random?\n\n" \
                    "{1: 'I would like to choose this name!'\n" \
                    " 2: 'I would like to choose another one at random'\n" \
